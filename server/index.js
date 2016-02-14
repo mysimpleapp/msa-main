d61618fc04df45767236a69be8689437f0092bca
Nicolas CARREZ <nicolas.carrez@gmail.com>
2016-02-14 16:11:31 +0100

    Add partial route + Read msa-navmenu content server side

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
-var mainApp = module.exports = App.subApp();
+var mainApp = module.exports = App.subApp()
+
+var navmenuApp = require("../../msa-navmenu/server")
 
 // template
 var fs = require('fs');
@@ -6,29 +8,50 @@ var mustache = require('mustache');
 var template = fs.readFileSync(__dirname+'/views/index.html', "utf8");
 mustache.parse(template);
 
+mainApp.get("*", function(req, res, next) {
+	next();
+});
+
 // default route
 mainApp.get("/", function(req, res, next) {
-	req.url = "/sheet";
-	req.query = { key:"home" }
+	req.url = "/page/home";
 	next();
 });
 
 // import some middlewares
 App.use(require("body-parser").json());
 
+// partialApp
+var partialApp = App.subApp();
+mainApp.use("/partial", partialApp);
+
 // subApp routing
 for(var routeName in App.routes) {
 	var subApp = App.getSubAppFromRouteName(routeName);
-	if(subApp) mainApp.use("/"+routeName, subApp);
+	if(subApp) {
+		mainApp.use("/"+routeName, subApp);
+		partialApp.use("/"+routeName, subApp);
+	}
 }
 
 // render view with partial content
 mainApp.get('*', function(req, res, next) {
 	if(!res.partial) return next();
-	res.setHeader('content-type', 'text/html');
-	var html = mustache.render(template, {partial:res.partial});
-	res.send(html);
-});
+	var contentPartial = res.partial
+	navmenuApp.getAsPartial(req, res, function(){
+		if(!res.partial) return next();
+		var headerPartial = res.partial
+		res.setHeader('content-type', 'text/html')
+		var html = mustache.render(template, {headerPartial:headerPartial, contentPartial:contentPartial})
+		res.send(html)
+	})
+})
+
+// render partial as ajax
+partialApp.get("*", function(req, res, next) {
+	if(!res.partial) return next();
+	res.json(res.partial)
+})
 
 // static routing
 mainApp.use(App.express.static(App.dirname));
